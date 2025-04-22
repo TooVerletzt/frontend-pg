7d9335b41ec236e5bcdec63cc5e2a31fd6b055bc
TooVerletzt <zS23004724@estudiantes.uv.mx>
2025-04-22 10:15:16 -0600

    actualizacion

--- callrestapi.js
+++ callrestapi.js
@@ -1,58 +1,146 @@
-var url = "http://localhost:3300/api/users";
+var url = "https://pg-restapi-axzt.onrender.com/api/users";
+let selectedUserId = null;
 
 function postUser() {
-  console.log(url);
+    console.log(url);
 
-  var myName = $('#name').val();
-  var myEmail = $('#email').val();
-  var myAge = $('#age').val();
-  var myComments = $('#comments').val();
+    var myName = $('#name').val();
+    var myEmail = $('#email').val();
+    var myAge = $('#age').val();
+    var myComments = $('#comments').val();
 
-  console.log({
-    name: myName,
-    email: myEmail,
-    age: myAge,
-    comments: myComments
-  });
+    if (!myName || !myEmail || !myAge || !myComments) {
+        alert('Por favor completa todos los campos');
+        return;
+    }
 
-  $.ajax({
-    url: url,
-    type: 'post',
-    dataType: 'json',
-    contentType: 'application/json',
-    success: function (data) {
-      $('#resultado').html(JSON.stringify(data.user));
-    },
-    data: JSON.stringify({
-      name: myName,
-      email: myEmail,
-      age: myAge,
-      comments: myComments
-    })
-  });
-}
-function getUsers() {
-  console.log(url);
+    var myuser = {
+        name: myName,
+        email: myEmail,
+        age: myAge,
+        comments: myComments
+    };
+    console.log(myuser);
 
-  $.getJSON(url, function(json) {
-    console.log(json);
-
-    var arrUsers = json.users;
-    var htmlTableUsers = '<table border="1">';
-
-    arrUsers.forEach(function(item) {
-      console.log(item);
-      htmlTableUsers += '<tr>' +
-        '<td>' + item.id + '</td>' +
-        '<td>' + item.name + '</td>' +
-        '<td>' + item.email + '</td>' +
-        '<td>' + item.age + '</td>' +
-        '<td>' + item.comments + '</td>' +
-      '</tr>';
+    $.ajax({
+        url: url,
+        type: 'post',
+        dataType: 'json',
+        contentType: 'application/json',
+        data: JSON.stringify(myuser),
+        success: function (data) {
+            console.log(data);
+            alert('Usuario agregado correctamente');
+            clearForm();  
+            $('#resultado').html(JSON.stringify(data.user));
+        },
+       
     });
-
-    htmlTableUsers += '</table>';
-    $('#resultado').html(htmlTableUsers);
-  });
 }
 
+function getUsers() {
+    console.log(url);
+
+    $.getJSON(url, function(json) {
+        console.log(json);
+
+        var arrUsers = json.users;
+
+        var htmlTableUsers = '<table border="1">';
+
+        arrUsers.forEach(function(item, index) {
+            console.log(item);
+            htmlTableUsers += `
+                <tr>
+                    <td>${index + 1}</td>
+                    <td>${item.name}</td>
+                    <td>${item.email}</td>
+                    <td>${item.age}</td>
+                    <td>${item.comments}</td>
+                <td>
+                <button onclick="deleteUser(${item.id})">Eliminar</button>
+                <button onclick="fillForm(${item.id}, '${item.name}', '${item.email}', ${item.age}, '${item.comments}')">Editar</button>
+
+            </td>
+        </tr>
+    `;
+});
+     htmlTableUsers += '</table>';
+
+        $('#resultado').html(htmlTableUsers);
+    });
+}
+
+function deleteUser(id) {
+    const confirmar = confirm("¿Deseas eliminar este usuario?");
+    if (!confirmar) return;
+
+    $.ajax({
+        url: `${url}/${id}`,
+        type: 'DELETE',
+        success: function () {
+            alert('Usuario eliminado');
+            getUsers();
+        },
+        error: function (err) {
+            console.error('Error al eliminar:', err);
+        }
+    });
+}
+function updateUser() {
+    if (!selectedUserId) {
+        alert("Primero selecciona un usuario con el botón Editar");
+        return;
+    }
+
+    const name = $('#name').val();
+    const email = $('#email').val();
+    const age = $('#age').val();
+    const comments = $('#comments').val();
+
+    if (!name || !email || !age || !comments) {
+        alert("Completa todos los campos para actualizar");
+        return;
+    }
+
+    const updatedUser = { name, email, age, comments };
+
+    $.ajax({
+        url: `${url}/${selectedUserId}`,
+        type: 'PUT',
+        contentType: 'application/json',
+        data: JSON.stringify(updatedUser),
+        success: function () {
+            alert("Usuario actualizado correctamente");
+            getUsers();
+            clearForm();
+            $('#updateBtn').hide(); // ocultar botón actualizar
+            selectedUserId = null;
+        },
+        error: function (err) {
+            console.error('Error al actualizar:', err);
+        }
+    });
+}
+
+function fillForm(id, name, email, age, comments) {
+    $('#name').val(name);
+    $('#email').val(email);
+    $('#age').val(age);
+    $('#comments').val(comments);
+    selectedUserId = id;
+
+    $('#updateBtn').show(); // muestra botón actualizar
+}
+
+
+
+// Limpiar inputs
+function clearForm() {
+    $('#name').val('');
+    $('#email').val('');
+    $('#age').val('');
+    $('#comments').val('');
+    selectedUserId = null;
+    $('#updateBtn').hide(); 
+}
